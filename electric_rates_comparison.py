037ebc697c912949509356f191aa963c4e5b45f4
hoho <kumass2021@gmail.com>
2022-01-11 10:57:58 +0900

    data config completed

--- electric_rates_comparison.py
+++ electric_rates_comparison.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import pandas as pd
+from datetime import datetime
 from MonthCalc import *
 
 # POST (JSON)
@@ -11,40 +12,50 @@ user_data = month_calc.user_data
 result_index = 1
 calc_type = month_calc.user_data.calc_type
 
+dt_parser = lambda x: datetime.strptime(x[:-6], "%Y-%m-%d %H:%M:%S")
+
 
 def get_data_list():
-    data_df = pd.read_csv('future_5min_total.csv', index_col='date', parse_dates=True, delimiter=',')
-    months = [g for n, g in data_df.groupby(pd.Grouper(freq='M'))]
-    print(months)
+    data_df = pd.read_csv('future_5min_total.csv',
+                          index_col=['date'],
+                          parse_dates=['date'],
+                          date_parser=dt_parser,
+                          infer_datetime_format=True,
+                          delimiter=',')
+    months_df = [g for n, g in data_df.groupby(pd.Grouper(freq='M'))]
+    print(months_df)
 
-    print(len(months))
-    print(months[0])
+    print(len(months_df))
+    print(months_df[0])
 
-    for month in months:
-        target_month = month[pd.DatetimeIndex(month.index).month]
-        if calc_type == 0:      # 주택용
+    for month_df in months_df:
+        print(type(month_df))
+        target_month = month_df.index.month[0]
+        if calc_type == 0:  # 주택용
             # 단순 합산 후 데이터 input
-            month['total1'].resample('1M').sum()
-        else:       # 일반용, 산업용
-            off_peak = month[(pd.DatetimeIndex(month.index).hour < 9) | (pd.DatetimeIndex(month.index).hour >= 23)]
+            month_df['total1'].resample('1M').sum()
+        else:  # 일반용, 산업용
+            off_peak = month_df[
+                (month_df.index.hour < 9) | (month_df.index.hour >= 23)]
             if 2 <= target_month < 11:
-                mid_peak = month[(pd.DatetimeIndex(month.index).hour >= 9) | (pd.DatetimeIndex(month.index).hour < 10)  |
-                                 (pd.DatetimeIndex(month.index).hour >= 12) | (pd.DatetimeIndex(month.index).hour < 13) |
-                                 (pd.DatetimeIndex(month.index).hour >= 17) | (pd.DatetimeIndex(month.index).hour < 23)]
-                on_peak = month[(pd.DatetimeIndex(month.index).hour >= 10) | (pd.DatetimeIndex(month.index).hour < 12) |
-                                (pd.DatetimeIndex(month.index).hour >= 13) | (pd.DatetimeIndex(month.index).hour < 17) ]
+                mid_peak = month_df[
+                    (month_df.index.hour >= 9) | (month_df.index.hour < 10) |
+                    (month_df.index.hour >= 12) | (month_df.index.hour < 13) |
+                    (month_df.index.hour >= 17) | (month_df.index.hour < 23)]
+                on_peak = month_df[
+                    (month_df.index.hour >= 10) | (month_df.index.hour < 12) |
+                    (month_df.index.hour >= 13) | (month_df.index.hour < 17)]
             else:
-                mid_peak = month[(pd.DatetimeIndex(month.index).hour >= 9) | (pd.DatetimeIndex(month.index).hour < 10)  |
-                                 (pd.DatetimeIndex(month.index).hour >= 12) | (pd.DatetimeIndex(month.index).hour < 13) |
-                                 (pd.DatetimeIndex(month.index).hour >= 17) | (pd.DatetimeIndex(month.index).hour < 23)]
-                on_peak = month[(pd.DatetimeIndex(month.index).hour >= 10) | (pd.DatetimeIndex(month.index).hour < 12) |
-                                (pd.DatetimeIndex(month.index).hour >= 13) | (pd.DatetimeIndex(month.index).hour < 17) ]
+                mid_peak = month_df[
+                    (month_df.index.hour >= 9) | (month_df.index.hour < 10) |
+                    (month_df.index.hour >= 12) | (month_df.index.hour < 13) |
+                    (month_df.index.hour >= 17) | (month_df.index.hour < 23)]
+                on_peak = month_df[
+                    (month_df.index.hour >= 10) | (month_df.index.hour < 12) |
+                    (month_df.index.hour >= 13) | (month_df.index.hour < 17)]
 
 
 get_data_list()
-
-
-
 
 # 비교 세트, 일반용과 산업용에 적용 가능
 # a number = class1 * 100 + class2 * 10 + class3 * 1
@@ -54,7 +65,7 @@ comparison_set3 = (200, 201, 202)
 comparison_set4 = (210, 211, 212)
 comparison_set5 = (220, 221, 222)
 comparison_sets = [comparison_set1, comparison_set2, comparison_set3,
-                comparison_set4, comparison_set5]
+                   comparison_set4, comparison_set5]
 
 selector = user_data.class1 * 100 + user_data.class2 * 10 + user_data.class_contract
 
@@ -69,17 +80,18 @@ for i in range(5):
             target_set_idx = i
 
 # json file import.
-with open ("electric_rates.json", "r", encoding="utf-8") as f_rates_json:
+with open("electric_rates.json", "r", encoding="utf-8") as f_rates_json:
     rates_data = json.load(f_rates_json)
     f_rates_json.close()
 
 # 현재 요금제 기반 요금계산
 for i in range(1, 13):
-    result_list[i-1] = month_calc.get_result(user_data, i)
+    result_list[i - 1] = month_calc.get_result(user_data, i)
 
 # 요금 데이터로부터 계산할 요금의 세부정보 얻기
 target_gen_obj = (item for item in rates_data['electric_rates'] if item['calc_type'] == calc_type and item['class1']
-                    == user_data.class1 and item['class2'] == user_data.class2 and item['class_contract'] == user_data.class_contract)
+                  == user_data.class1 and item['class2'] == user_data.class2 and item[
+                      'class_contract'] == user_data.class_contract)
 for value in target_gen_obj:
     target_rates_dict = value
 rates_title = target_rates_dict['description']
@@ -89,7 +101,7 @@ data = {'title': 'result', 'id': 0, 'selector': selector,
 res = requests.post('http://127.0.0.1:3000',
                     data=json.dumps(data), headers=headers)
 
-if target_set_idx is not None:      # 비교군이 존재하는 요금일 경우.
+if target_set_idx is not None:  # 비교군이 존재하는 요금일 경우.
 
     # 현재 사용하는 요금을 비교군에서 제거
     target_sets = list(comparison_sets[target_set_idx])
@@ -101,19 +113,22 @@ if target_set_idx is not None:      # 비교군이 존재하는 요금일 경우
         user_data.class_contract = (comparable_selector % 10)
 
         # 요금 데이터로부터 계산할 요금의 세부정보 얻기
-        target_gen_obj = (item for item in rates_data['electric_rates'] if item['calc_type'] == calc_type and item['class1']
-                    == user_data.class1 and item['class2'] == user_data.class2 and item['class_contract'] == user_data.class_contract)
+        target_gen_obj = (item for item in rates_data['electric_rates'] if
+                          item['calc_type'] == calc_type and item['class1']
+                          == user_data.class1 and item['class2'] == user_data.class2 and item[
+                              'class_contract'] == user_data.class_contract)
         for value in target_gen_obj:
             target_rates_dict = value
         rates_title = target_rates_dict['description']
 
         # 1년(12개월)에 해당하는 계산 
         for i in range(1, 13):
-            result_list[i-1] = month_calc.get_result(user_data, i)
-        
-        data = {'title': 'result', 'id': result_index, 'selector': comparable_selector, 'rates_title': rates_title, 'result_list': result_list}
+            result_list[i - 1] = month_calc.get_result(user_data, i)
+
+        data = {'title': 'result', 'id': result_index, 'selector': comparable_selector, 'rates_title': rates_title,
+                'result_list': result_list}
         res = requests.post('http://127.0.0.1:3000', data=json.dumps(data), headers=headers)
-        
+
         result_index += 1
 else:
     print("not included")
@@ -123,8 +138,7 @@ else:
     # res = requests.post('http://127.0.0.1:3000',
     #                     data=json.dumps(data), headers=headers)
 
-
-# 리스트에 있는대로 서버에 API 전달 
+# 리스트에 있는대로 서버에 API 전달
 
 # 현재 사용중인 요금제는 리스트에서 제거
 
@@ -133,4 +147,3 @@ else:
 # data = {'title': 'finish'}
 # res = requests.post('http://127.0.0.1:3000', data=json.dumps(data), headers=headers)
 print(str(res.status_code) + " | " + res.text)
-
